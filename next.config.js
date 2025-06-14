0f6e562e2ae2f4202c0b0f12eb50f34b8f3485ee
mmmatolde <matoldecouto@gmail.com>
2025-06-14 17:56:56 +0100

    atualiza: adiciona configuração de domínios permitidos para imagens no Next.js, permitindo o carregamento de imagens do Contentful

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['contentful', '@contentful/rich-text-types'],
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
